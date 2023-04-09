8233bb8f7de2504b0b5c1945f82884c0f60e8cdd
John Wanjala <43532333+G-ony@users.noreply.github.com>
2023-04-09 21:14:26 +0300

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-"""
-Module say_my_name
-Prints out first name and last name
-"""
-
-
 def say_my_name(first_name, last_name=""):
-    """
-    Prints out first name and last name
-    first name and last name must be of the type string
-    or error if not string
-    """
-
-    if type(first_name) is not str:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""
-Module text_indentation
-prints a text with 2 new lines
-Adds 2 new lines after a set of characters.
-"""
+"""text indent"""
 
 
 def text_indentation(text):
-    """
-    Prints a text and add two newlines
-    after each of these characters {'.', '?', ':'}.
-    """
-
-    if type(text) is not str:
+    """text indent"""
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    for delim in ".:?":
-        text = (delim + "\n\n").join(
-            [line.strip(" ") for line in text.split(delim)])
+    for delimeter in "?:.":
+        words = (delimeter + "\n\n").join(
+                [index.strip(" ") for index in words.split(delimeter)])
 
-    print("{}".format(text), end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
